5d83f64de93e9f37fe3ed066f7e0a0cf9639fa71
StruggleWithJava <giordano.sabbioni@students.fhnw.ch>
2020-05-27 18:02:25 +0200

    MovieListEditActivity finished

--- app/src/main/java/ch/fhnw/movie4me/ui/movielists/MovieListsFragment.java
+++ app/src/main/java/ch/fhnw/movie4me/ui/movielists/MovieListsFragment.java
@@ -1,34 +1,68 @@
 package ch.fhnw.movie4me.ui.movielists;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 import ch.fhnw.movie4me.R;
 import ch.fhnw.movie4me.db.MovieListDb;
 import ch.fhnw.movie4me.dto.MovieList;
+import ch.fhnw.movie4me.ui.MovieListEditActivity;
 
 public class MovieListsFragment extends Fragment {
 
+    private View rootView;
+
     private MovieListDb movieListDb;
+    private FloatingActionButton floatingActionButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_movielists, container, false);
-
+        this.rootView = inflater.inflate(R.layout.fragment_movielists, container, false);
         this.movieListDb = new MovieListDb();
+
+        this.refreshFields();
+
+        floatingActionButton = this.rootView.findViewById(R.id.floatingActionButtonAdd);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openListEditActivity();
+            }
+        });
+
+        return this.rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        this.refreshFields();
+    }
+
+    private void refreshFields() {
         List<MovieList> movieLists = this.movieListDb.getAll();
 
-        //TEMP
-        final TextView textView = root.findViewById(R.id.text_movielists);
+        final TextView textView = this.rootView.findViewById(R.id.text_movielists);
         textView.setText("TEMP: I have found " + movieLists.size() + " lists.");
-
-        return root;
     }
+
+    public void openListEditActivity() {
+        Intent intent = new Intent(getView().getContext(), MovieListEditActivity.class);
+        startActivity(intent);
+    }
+
+
 }
