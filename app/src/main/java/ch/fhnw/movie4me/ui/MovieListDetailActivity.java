268afde91481021b6ae684cc742c3e235544f6ab
simonkwg <36926589+simonkwg@users.noreply.github.com>
2020-05-30 20:54:27 +0200

    Update MovieListDetailActivity.java
    MovieListDetailActivity updated.

--- app/src/main/java/ch/fhnw/movie4me/ui/MovieListDetailActivity.java
+++ app/src/main/java/ch/fhnw/movie4me/ui/MovieListDetailActivity.java
@@ -1,15 +1,21 @@
 package ch.fhnw.movie4me.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import ch.fhnw.movie4me.R;
+import ch.fhnw.movie4me.adapter.MovieArrayAdapter;
 import ch.fhnw.movie4me.db.MovieListDb;
 import ch.fhnw.movie4me.db.MovieListDetailDb;
 import ch.fhnw.movie4me.dto.Movie;
@@ -29,6 +35,9 @@ public class MovieListDetailActivity extends AppCompatActivity {
     private List<MovieListDetail> movieListDetails;
     private List<Movie> movies;
 
+    private Button btnEditList;
+    private Button btnDeleteMovies;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,11 +56,34 @@ public class MovieListDetailActivity extends AppCompatActivity {
             this.movieList = this.movieListDb.get(movieListId);
             this.movieListDetails = this.movieListDetailDb.getByMovieListId(movieListId);
 
-            for(MovieListDetail detail : this.movieListDetails) {
-                Movie movie = this.theMovieDbClient.getMovie((int)detail.getMovieId());
+            for (MovieListDetail detail : this.movieListDetails) {
+                Movie movie = this.theMovieDbClient.getMovie((int) detail.getMovieId());
                 this.movies.add(movie);
             }
+
+            final TextView textTitle = findViewById(R.id.txMovieListDescription);
+            textTitle.setText("description placeholder" /* + this.movieList.getDescription()*/);
+
+            final RecyclerView lvMoviesList = findViewById(R.id.rvMovieListDetail);
+            lvMoviesList.setHasFixedSize(true);
+            lvMoviesList.setLayoutManager(new LinearLayoutManager(this));
+
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setTitle("My List: Listname");
+            actionBar.setDisplayHomeAsUpEnabled(false);
+
+            this.theMovieDbClient = TheMovieDbClient.getInstance();
+            List<Movie> movies = this.theMovieDbClient.searchMovie("Star Wars");
+
+            MovieArrayAdapter itemArrayAdapter = new MovieArrayAdapter(this, movies);
+            lvMoviesList.setAdapter(itemArrayAdapter);
+
         }
 
+    }
+
+    public void click() {
+        Intent intent = new Intent(this, MovieListEditActivity.class);
+        startActivity(intent);
     }
 }
